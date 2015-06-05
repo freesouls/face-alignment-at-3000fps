42c3a7da8769f506b5342b6b6a377cc8f90c6429
freesouls <declanxu@gmail.com>
2015-06-05 20:21:03 +0800

    update

---
+++ release/CMakeFiles/application.dir/DependInfo.cmake
@@ -0,0 +1,35 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/dirk/working/face-alignment-at-3000fps/liblinear/blas/daxpy.c" "/Users/dirk/working/face-alignment-at-3000fps/release/CMakeFiles/application.dir/Users/dirk/working/face-alignment-at-3000fps/liblinear/blas/daxpy.c.o"
+  "/Users/dirk/working/face-alignment-at-3000fps/liblinear/blas/ddot.c" "/Users/dirk/working/face-alignment-at-3000fps/release/CMakeFiles/application.dir/Users/dirk/working/face-alignment-at-3000fps/liblinear/blas/ddot.c.o"
+  "/Users/dirk/working/face-alignment-at-3000fps/liblinear/blas/dnrm2.c" "/Users/dirk/working/face-alignment-at-3000fps/release/CMakeFiles/application.dir/Users/dirk/working/face-alignment-at-3000fps/liblinear/blas/dnrm2.c.o"
+  "/Users/dirk/working/face-alignment-at-3000fps/liblinear/blas/dscal.c" "/Users/dirk/working/face-alignment-at-3000fps/release/CMakeFiles/application.dir/Users/dirk/working/face-alignment-at-3000fps/liblinear/blas/dscal.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "Clang")
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/dirk/working/face-alignment-at-3000fps/liblinear/linear.cpp" "/Users/dirk/working/face-alignment-at-3000fps/release/CMakeFiles/application.dir/Users/dirk/working/face-alignment-at-3000fps/liblinear/linear.cpp.o"
+  "/Users/dirk/working/face-alignment-at-3000fps/liblinear/tron.cpp" "/Users/dirk/working/face-alignment-at-3000fps/release/CMakeFiles/application.dir/Users/dirk/working/face-alignment-at-3000fps/liblinear/tron.cpp.o"
+  "/Users/dirk/working/face-alignment-at-3000fps/main.cpp" "/Users/dirk/working/face-alignment-at-3000fps/release/CMakeFiles/application.dir/Users/dirk/working/face-alignment-at-3000fps/main.cpp.o"
+  "/Users/dirk/working/face-alignment-at-3000fps/randomforest.cpp" "/Users/dirk/working/face-alignment-at-3000fps/release/CMakeFiles/application.dir/Users/dirk/working/face-alignment-at-3000fps/randomforest.cpp.o"
+  "/Users/dirk/working/face-alignment-at-3000fps/regressor.cpp" "/Users/dirk/working/face-alignment-at-3000fps/release/CMakeFiles/application.dir/Users/dirk/working/face-alignment-at-3000fps/regressor.cpp.o"
+  "/Users/dirk/working/face-alignment-at-3000fps/utils.cpp" "/Users/dirk/working/face-alignment-at-3000fps/release/CMakeFiles/application.dir/Users/dirk/working/face-alignment-at-3000fps/utils.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/local/include/opencv"
+  "/usr/local/include"
+  )
+set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ release/CMakeFiles/application.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/application.dir/Users/dirk/working/face-alignment-at-3000fps/liblinear/blas/daxpy.c.o"
+  "CMakeFiles/application.dir/Users/dirk/working/face-alignment-at-3000fps/liblinear/blas/ddot.c.o"
+  "CMakeFiles/application.dir/Users/dirk/working/face-alignment-at-3000fps/liblinear/blas/dnrm2.c.o"
+  "CMakeFiles/application.dir/Users/dirk/working/face-alignment-at-3000fps/liblinear/blas/dscal.c.o"
+  "CMakeFiles/application.dir/Users/dirk/working/face-alignment-at-3000fps/liblinear/tron.cpp.o"
+  "CMakeFiles/application.dir/Users/dirk/working/face-alignment-at-3000fps/liblinear/linear.cpp.o"
+  "CMakeFiles/application.dir/Users/dirk/working/face-alignment-at-3000fps/utils.cpp.o"
+  "CMakeFiles/application.dir/Users/dirk/working/face-alignment-at-3000fps/randomforest.cpp.o"
+  "CMakeFiles/application.dir/Users/dirk/working/face-alignment-at-3000fps/regressor.cpp.o"
+  "CMakeFiles/application.dir/Users/dirk/working/face-alignment-at-3000fps/main.cpp.o"
+  "application.pdb"
+  "application"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/application.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
